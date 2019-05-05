0e542a497a1c3ece1cf2b605f51974771746cde5
NabeelZanabeet <nabeelzanabeet@gmail.com>
2019-05-05 15:31:17 +0300

    Print static respone

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello '+ req.query.name)
+  res.send('Hello ')
 });
 
 app.listen(8000, () => {
