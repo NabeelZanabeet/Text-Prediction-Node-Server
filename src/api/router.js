2fffd18239db227e98d56ea85281a7703928c753
Haya abu al halawa <hayaah@wix.com>
2019-05-12 10:28:01 +0300

    Add Default Route

--- src/api/router.js
+++ src/api/router.js
@@ -4,5 +4,6 @@ import predictionController from './controller';
 const router = express.Router();
 
 router.get('/api/:text/:cursorPosition/:lineNum', predictionController.mainResponse);
+router.get('/', predictionController.mainPage);
 
 export default router;
